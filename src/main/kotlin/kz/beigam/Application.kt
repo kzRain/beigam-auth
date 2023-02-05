b356cad20049711787d052db70b9fe0ac97e09ae
rus <ruslan.beyganov@gmail.com>
2023-02-05 16:44:39 +0600

    Initial Commit

---
+++ src/main/kotlin/kz/beigam/Application.kt
@@ -0,0 +1,12 @@
+package kz.beigam
+
+import io.ktor.server.application.*
+import kz.beigam.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+}
